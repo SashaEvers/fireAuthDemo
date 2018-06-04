a6c014ccac4f91594dc442879e3e3a4d95b7ad97
SashaEvers <sashaevers@hotmail.com>
2018-06-04 12:35:45 +0100

    FireAuth
    Firebase Authentication

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,11 @@
+export const FIREBASE_CONFIG = {
+
+    apiKey: "AIzaSyCRifVCcNBoVbvB0AkMleBF49YYNYWY3Vc",
+    authDomain: "fireauthdemo-2b4a9.firebaseapp.com",
+    databaseURL: "https://fireauthdemo-2b4a9.firebaseio.com",
+    projectId: "fireauthdemo-2b4a9",
+    storageBucket: "fireauthdemo-2b4a9.appspot.com",
+    messagingSenderId: "298355579291"
+
+
+}
